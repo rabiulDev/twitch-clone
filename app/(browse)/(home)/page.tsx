3e8a9ce18beb71a44f3bae45bd31be1ec76c9431
rabiulDev <rabiul.dev@gmail.com>
2024-01-21 17:19:05 +0600

    actions added in navbar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1>Home page</h1>
     </div>
   );
 };
--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Logo } from "./logo";
 import { Search } from "./search";
+import { Actions } from "./actions";
 
 type Props = {};
 
@@ -9,6 +10,7 @@ export const Navbar = (props: Props) => {
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
       <Logo />
       <Search />
+      <Actions />
     </nav>
   );
 };
