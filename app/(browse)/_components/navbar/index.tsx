f4996d78c4aeedbef0f763ec761408499c04682a
rabiulDev <rabiul.dev@gmail.com>
2024-01-21 16:42:56 +0600

    search box added in navbar

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Logo } from "./logo";
+import { Search } from "./search";
 
 type Props = {};
 
@@ -7,6 +8,7 @@ export const Navbar = (props: Props) => {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
       <Logo />
+      <Search />
     </nav>
   );
 };
