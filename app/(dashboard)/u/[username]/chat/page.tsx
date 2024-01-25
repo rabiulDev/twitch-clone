378cdf2b2c21ee6dee7d5ad335ae658fd8ce68d1
rabiulDev <rabiul.dev@gmail.com>
2024-01-25 19:12:09 +0600

    chat page added

---
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const ChatPage = (props: Props) => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Chat settings</h1>
+      </div>
+    </div>
+  );
+};
+export default ChatPage;
