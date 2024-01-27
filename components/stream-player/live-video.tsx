1ad961360217d6adb1e149ece45e40bae976674a
rabiulDev <rabiul.dev@gmail.com>
2024-01-27 19:39:33 +0600

    live video component modified

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -10,5 +10,9 @@ interface LiveVideoProps {
 }
 
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
-  return <div>Live Video</div>;
+  return (
+    <div className="h-full flex flex-col space-y-4 justify-center items-center">
+      <p className="text-muted-foreground">Live Video</p>
+    </div>
+  );
 };
