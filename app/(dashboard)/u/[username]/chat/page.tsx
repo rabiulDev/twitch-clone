a78df2698e77a0994f1308c06a595461fbf502d0
rabiulDev <rabiul.dev@gmail.com>
2024-01-27 11:02:10 +0600

    Stream not found error added

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -7,6 +7,11 @@ type Props = {};
 const ChatPage = async (props: Props) => {
   const self = await getSelf();
   const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
+
   return (
     <div className="p-6">
       <div className="mb-4">
