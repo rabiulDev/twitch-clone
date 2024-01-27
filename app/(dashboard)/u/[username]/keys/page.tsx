b1aacb3cc399a0fa3bb633d650e6b6d7ba3cec3a
rabiulDev <rabiul.dev@gmail.com>
2024-01-27 13:26:30 +0600

    Generate connection modal added

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -3,6 +3,7 @@ import { getStreamByUserId } from "@/lib/stream-service";
 import { Button } from "@/components/ui/button";
 import { UrlCard } from "./_components/url-card";
 import { KeyCard } from "./_components/key-card";
+import { ConnectModal } from "./_components/connect-modal";
 
 const KeysPage = async () => {
   const self = await getSelf();
@@ -15,7 +16,7 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & URLs</h1>
-        <Button variant={"primary"}>Generate</Button>
+        <ConnectModal />
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />
