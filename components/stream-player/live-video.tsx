cb39b5e25e9768bce3c64c394a4f3be796055b17
rabiulDev <rabiul.dev@gmail.com>
2024-01-28 13:49:52 +0600

    volume controller added

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -5,6 +5,7 @@ import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
 import { useEventListener } from "usehooks-ts";
 import { FullscreenControl } from "./fullscreen-control";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -15,6 +16,29 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const [isFullscreen, setFullscreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+    setVolume(isMuted ? 50 : 0);
+
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, []);
 
   const toggleFullscreen = () => {
     if (isFullscreen) {
@@ -44,6 +68,11 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <VolumeControl
+            onChange={onVolumeChange}
+            onToggle={toggleMute}
+            value={volume}
+          />
           <FullscreenControl
             isFullscreen={isFullscreen}
             onToggle={toggleFullscreen}
